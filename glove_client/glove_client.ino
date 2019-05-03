5d35835eb5138cd62c17399c288c944f57d0fd9f
Will Farris <9190155+GetLiquid@users.noreply.github.com>
2019-05-02 19:06:55 -0700

    asdf

--- glove_client/glove_client.ino
+++ glove_client/glove_client.ino
@@ -7,144 +7,166 @@
 #include <SoftwareSerial.h>
 
 //Some general configurations for the gloves
-#define NUMPIXELS 5       //set the number of pixels on each strip
-#define BUFFERSIZE 15     //set the size of the data buffer to read from the phone
-#define SERIALBAUD 38400  //set the baud rate for serial communication with the bluetooth adapter
+#define NUMPIXELS 5        //set the number of pixels on each strip
+#define BUFFERSIZE 4       //set the size of the data buffer to read from the phone
+#define SERIALBAUD 115200  //set the baud rate for serial communication with the bluetooth adapter
 
 //define the pins used
+//#define GEMMA
+#ifdef GEMMA
+
 #define LED_PIN 1  //the data pin for the LED strip
 #define BT_RX 0    //Connect bluetooth TX to this pin (green)
 #define BT_TX 2    //Connect bluetooth RX to this pin (yellow)
 
+#else
+
+#define LED_PIN 4
+#define BT_RX   3
+#define BT_TX   5
+
+#endif
+
+#define START_CODE  0xCF
+#define END_CODE    0xFC
 #define SET_RGB_ALL 0x0A
 
 //Some important variables
 byte buf[BUFFERSIZE]; //this array of bytes stores the data from the bluetooth module each time data is read
-struct light
-{
-  uint8_t r, g, b;
-} fingers[NUMPIXELS];
+byte new_data = 0;
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, LED_PIN, NEO_GRB + NEO_KHZ800);
 SoftwareSerial bt_data = SoftwareSerial(BT_RX,BT_TX);
 
 void setup() {
+
+#ifndef GEMMA
+  Serial.begin(SERIALBAUD);
+  //Serial.setTimeout(10);
+#endif
+  
   bt_data.begin(SERIALBAUD); //begin the serial connection
-  bt_data.setTimeout(5);
+  //bt_data.setTimeout(10);
 
-  strip.begin(); //initialize the strip of neopixels
-  for(int i=0;i<BUFFERSIZE;++i)
-  {
-    buf[i] = 0;
-  }
-  for(int i=0;i<NUMPIXELS;++i)
-  {
-    fingers[i].r = 0;
-    fingers[i].g = 0;
-    fingers[i].b = 0;
-  }
-
+  strip.begin();
   for(uint8_t i = 0;i<NUMPIXELS;++i)
   {
     strip.setPixelColor(i, 200, 100, 100);
   }
+  strip.setBrightness(75);
   strip.show();
   
 }
 
 void loop() {
-  bt_data.write(0xFF);
-  bt_data.write(0xCC);
-  delay(1);
-  if(bt_data.available())
+  /*recv_data();
+  if(new_data)
   {
-    bt_data.readBytes(buf, BUFFERSIZE); //read BUFFERSIZE bytes from the serial and store them into the buffer
-  }
-    
-  /*switch(buf[BUFFERSIZE-1])
+    if(buf[3]) < 5)
+    {
+      display_lights();
+    }
+  } else
   {
-    case SET_RGB_ALL:
-      for(int i=0;i<NUMPIXELS;++i)
-      {
-        fingers[i].r = buf[i*3];
-        fingers[i].g = buf[(i*3) + 1];
-        fingers[i].b = buf[(i*3) + 2];
-      }
-      break;
-    default:
-      /*fingers[0].r = 255;
-      fingers[0].g = 0;
-      fingers[0].b = 0;
-      break;
+    rainbowCycle(2);
   }*/
-  
-  for(uint8_t i=0;i<NUMPIXELS;++i)
-  {
-    strip.setPixelColor(i, fingers[i].r, fingers[i].g, fingers[i].b);
-  }
-  strip.show();
+  rainbowCycle(2);
 }
 
-/*void rainbowWheel()
-{
-    for(int i=0; i< NUMPIXELS; i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / NUMPIXELS)) & 255));
+void recv_data() {
+    static byte inProgress = 0;
+    static byte index = 0;
+    byte incoming;
+ 
+    while (bt_data.available() > 0 && !new_data) {
+        incoming = bt_data.read();
+
+        if (inProgress) {
+            if (incoming != END_CODE) {
+                buf[index] = incoming;
+                ++index;
+                if (index >= BUFFERSIZE) {
+                    index = BUFFERSIZE - 1;
+                }
+            }
+            else {
+                inProgress = false;
+                index = 0;
+                new_data = true;
+            }
+        }
+
+        else if (incoming == START_CODE) {
+            inProgress = true;
+        }
     }
 }
 
-void colorWheel()
+void print_data()
 {
-  for(int i=0;i<NUMPIXELS;++i)
+  if(new_data)
   {
-    strip.setPixelColor(i, Wheel(cycleOffset & 255));
+    Serial.print("Received... ");
+    for(int i=0;i<BUFFERSIZE;++i)
+    {
+      Serial.print(buf[i], HEX);
+      Serial.print(' ');
+    }
+    Serial.print('\n');
   }
-  cycleOffset += cycleVelocity;
+}
+
+void display_lights()
+{
+  if(new_data)
+  {
+    /*for(int i=0;i<NUMPIXELS;++i)
+    {
+      strip.setPixelColor(i, buf[(i*3)], buf[(i*3)+1], buf[(i*3)+2]);
+    }*/
+    strip.setPixelColor(buf[3], buf[1], buf[0], buf[2]);
+    new_data = false;
+    strip.show();
+  }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+  uint16_t i, j;
+
+  for(j=0; j<256 * 5; j++) { // 5 cycles of all colors on wheel
+    for(i=0; i< strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
 }
 
 // Input a value 0 to 255 to get a color value.
 // The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos)
-{
+uint32_t Wheel(byte WheelPos) {
   WheelPos = 255 - WheelPos;
-  if(WheelPos < 85)
-  {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-   }
-  else if(WheelPos < 170)
-  {
+  if(WheelPos < 85) {
+    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3,0);
+  }
+  if(WheelPos < 170) {
     WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3,0);
   }
-  else
-  {
-    WheelPos -= 170;
-    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  }
-}*/
-
-light Wheel(byte pos)
-{
-  light out;
-  pos = 255 - pos;
-  if(pos < 85)
-  {
-    out.r = 255 - pos * 3;
-    out.g = 0;
-    out.b = pos * 3;
-    return out;
-  } else if(pos < 170)
-  {
-    pos -= 85;
-    out.r = 0;
-    out.g = pos * 3;
-    out.b = 255 - pos * 3;
-    return out;
-  } else
-  {
-    pos -= 170;
-    out.r = pos * 3;
-    out.g = 255 - pos * 3;
-    out.b = 0;
-  }
-  
+  WheelPos -= 170;
+  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0,0);
 }
+
+uint8_t red(uint32_t c) {
+  return (c >> 16);
+}
+uint8_t green(uint32_t c) {
+  return (c >> 8);
+}
+uint8_t blue(uint32_t c) {
+  return (c);
+}
+
+
+
